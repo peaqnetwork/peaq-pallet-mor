d723163d41785ecdcc16b7c34c8abad997f0f89b
Michael Eberhardt <github@michael-eberhardt.de>
2023-01-26 15:10:30 +0200

    preparing config struct

--- src/types.rs
+++ src/types.rs
@@ -9,3 +9,15 @@ pub type CrtBalance<T> = <<T as crate::Config>::Currency as Currency<<T as frame
 // pub type CrtPosImbalance<T> = <<T as crate::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::PositiveImbalance;
 /// Short form type definition to simplify method definition.
 pub type CrtWeight<T> = <T as crate::Config>::WeightInfo;
+
+
+/// This struct defines the configurable paramters of the Peaq-MOR pallet. All contained
+/// parameters can be configured by a dispatchable function (extrinsic).
+#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
+pub struct MorConfig<Balance> {
+    /// How much tokens a machine owner gets rewarded, when registering a new machine on the network.
+    registration_reward: Balance,
+    /// Defines how much time is one period (to be online to get rewarded).
+    time_period_blocks: u8,
+}
