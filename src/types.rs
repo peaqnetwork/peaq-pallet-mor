ebfda0619671ba722d9c48508012461692697f4c
Michael Eberhardt <github@michael-eberhardt.de>
2023-01-26 18:21:35 +0200

    config type

--- src/types.rs
+++ src/types.rs
@@ -1,19 +1,25 @@
 //! All pallet relevant structs are defined here
 
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+use sp_core::RuntimeDebug;
 use frame_support::traits::Currency;
 
+
 /// Short form type definition to simplify method definition.
-pub type CrtBalance<T> =
-    <<T as crate::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
-// /// Short form type definition to simplify method definition.
-// pub type CrtPosImbalance<T> = <<T as crate::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::PositiveImbalance;
+pub type CrtBalance<T> = <<T as crate::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 /// Short form type definition to simplify method definition.
 pub type CrtWeight<T> = <T as crate::Config>::WeightInfo;
 
+
 /// This struct defines the configurable paramters of the Peaq-MOR pallet. All contained
 /// parameters can be configured by a dispatchable function (extrinsic).
-#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen)]
-#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(
+    PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug, TypeInfo //, MaxEncodedLen
+)]
 pub struct MorConfig<Balance> {
     /// How much tokens a machine owner gets rewarded, when registering a new machine on the network.
     registration_reward: Balance,
