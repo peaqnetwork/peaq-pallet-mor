61c0d48e0264dca664d7313bd00e427724629c53
Michael Eberhardt <github@michael-eberhardt.de>
2023-12-04 20:04:47 +0000

    fixed lib regarding StorageVersion

--- src/lib.rs
+++ src/lib.rs
@@ -200,8 +200,10 @@ pub mod pallet {
         };
     }
 
+    const STORAGE_VERSION: StorageVersion = StorageVersion::new(2);
+
     #[pallet::pallet]
-    #[pallet::without_storage_info]
+    #[pallet::storage_version(STORAGE_VERSION)]
     pub struct Pallet<T>(_);
 
     /// Configuration trait of this pallet.
