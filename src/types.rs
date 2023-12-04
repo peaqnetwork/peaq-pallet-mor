7d8fa8eb83f4bac2fbe09d79bb30761451703a01
Michael Eberhardt <github@michael-eberhardt.de>
2023-12-04 20:04:14 +0000

    fixed MorConfig regarding MaxEncodedLen

--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,9 @@
 //! All pallet relevant structs are defined here
 
-use codec::{Decode, Encode};
-use frame_support::traits::tokens::Balance as BalanceT;
-use frame_support::traits::Currency;
+use codec::{Decode, Encode, MaxEncodedLen};
+use frame_support::traits::{
+    tokens::Balance as BalanceT, Currency,
+};
 use scale_info::TypeInfo;
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
@@ -18,8 +19,10 @@ pub type WeightOf<T> = <T as crate::Config>::WeightInfo;
 /// This struct defines the configurable paramters of the Peaq-MOR pallet. All contained
 /// parameters can be configured by a dispatchable function (extrinsic).
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug, TypeInfo)]
-pub struct MorConfig<Balance> {
+#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+pub struct MorConfig<Balance> 
+where Balance: BalanceT + MaxEncodedLen
+{
     /// How much tokens a machine owner gets rewarded, when registering a new machine on the network.
     #[codec(compact)]
     pub registration_reward: Balance,
